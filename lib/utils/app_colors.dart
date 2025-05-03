2a72e87f511ad9473434517661a13b3910ca1888
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2025-05-03 10:55:41 -0300

    inicio colors tema dark

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Cores do tema claro
+  static const Color primary = Color(0xFF9867EC);
+  static const Color secondary = Color(0xFFFACF5A);
+  static const Color background = Color(0xFFF3F3F3);
+  static const Color text = Color(0xFF222222);
+
+  // Cores do tema escuro
+  static const Color darkPrimary = Color(0xFF7B52C1);
+  static const Color darkBackground = Color.fromRGBO(27, 28, 29, 0);
+  static const Color darkText = Color(0xFFFFFFFF);
+
+  // Cores adicionais (comuns)
+  static const Color error = Colors.red;
+  static const Color success = Colors.green;
+}
